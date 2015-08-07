bc0d37010267818fd2589db7c7d12fa981d0b7aa
Ben Earlam <bearlam@thoughtworks.com>
2015-08-07 14:12:53 +0100

    Adds input Parsing and first bit of command processing

--- src/clojure_mars_rover/core.clj
+++ src/clojure_mars_rover/core.clj
@@ -1,6 +1,48 @@
-(ns clojure-mars-rover.core)
+(ns clojure-mars-rover.core
+  (:require [clojure.string :as string]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- split-and-read
+  [string]
+  (-> string
+      (string/split #" ")
+      (#(map read-string %))))
+
+(defn- parse-plateau
+  [plateau]
+  (split-and-read plateau))
+
+(defn- parse-rover
+  [[start commands]]
+  {:start    (split-and-read start)
+   :commands (vec commands)})
+
+(defn- parse-rovers
+  [rovers]
+  (->> rovers
+       (partition 2)
+       (map parse-rover)))
+
+(defn parse-payload
+  [payload]
+  (let [[plateau & rovers] (string/split-lines payload)]
+    {:plateau (parse-plateau plateau)
+     :rovers  (parse-rovers rovers)}))
+
+(def movement-matrix
+  {'N [0 1]})
+
+(defn- process-command
+  [[x y dir :as position] command]
+  (let [[+x +y] (movement-matrix dir)]
+    [(+ x +x) (+ y +y) dir]))
+
+(defn- process-rover
+  [{:keys [start commands]} plateau]
+  (reduce process-command start commands))
+
+;(map read-string (string/split plateau #" "))
+(defn mars-landing
+  [payload]
+  (let [{:keys [plateau rovers]} (parse-payload payload)]
+    (for [rover rovers]
+      (process-rover rover plateau))))
--- test/clojure_mars_rover/core_test.clj
+++ test/clojure_mars_rover/core_test.clj
@@ -1,7 +1,17 @@
 (ns clojure-mars-rover.core-test
-  (:require [clojure.test :refer :all]
+  (:require [midje.sweet :refer :all]
             [clojure-mars-rover.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(facts "`mars-landing`"
+       (fact "it lands and moves north, incrementing the y axis"
+             (let [payload (str "5 5" "\n" "1 1 N" "\n" "M")]
+               (mars-landing payload) => '([1 2 N]))))
+
+(facts "`parse-payload"
+       (fact "it turns the payload into a map"
+             (let [payload (str "5 5" "\n" "1 1 N" "\n" "M" "\n" "1 2 S" "\n" "MLM")]
+               (parse-payload payload) => {:plateau [5 5]
+                                           :rovers  [{:start    [1 1 'N]
+                                                      :commands [\M]}
+                                                     {:start    [1 2 'S]
+                                                      :commands [\M\L\M]}]})))
